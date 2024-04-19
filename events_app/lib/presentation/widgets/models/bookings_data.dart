b1cc63a7977b0c03508d832968976b2494f64c0a
SalmanAliGithub <salmanalijijiga@gmail.com>
2024-04-19 04:48:46 +0300

    added booking model class

---
+++ events_app/lib/presentation/widgets/models/bookings_data.dart
@@ -0,0 +1,10 @@
+class BookData {
+  String eventName;
+  String eventPlace;
+  String eventDate;
+
+  BookData(
+      {required this.eventName,
+      required this.eventPlace,
+      required this.eventDate});
+}
