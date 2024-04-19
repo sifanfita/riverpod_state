1a18772b95959efb0ee1346cad5609d3e9686825
SalmanAliGithub <salmanalijijiga@gmail.com>
2024-04-19 04:52:36 +0300

    my bookings and users role assignment screens checked

--- events_app/lib/main.dart
+++ events_app/lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+// import 'presentation/screens/bookings_screen.dart';
+// import 'presentation/screens/splash_screen.dart';
+import 'package:events_app/presentation/screens/users_list_screen.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: UsersCardList(),
+  ));
+}
