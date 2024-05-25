d48a73323916d232bcfd6b0aa3ec697b2589a128
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-05-26 02:32:13 +0300

    Added Auth Model

---
+++ events_app/lib/models/auth_model.dart
@@ -0,0 +1,36 @@
+class LoginData {
+  final String email;
+  final String password;
+
+  LoginData({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
+
+class SignupData {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+
+  SignupData({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'password': password,
+    };
+  }
+}
