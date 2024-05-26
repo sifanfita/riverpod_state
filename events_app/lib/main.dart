0651a45c2d9c0486db87f438724b31354e2f4ee2
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-05-26 04:32:37 +0300

    Added Top Level Block Providers

--- events_app/lib/main.dart
+++ events_app/lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:events_app/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-// import 'presentation/screens/bookings_screen.dart';
-// import 'presentation/screens/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:events_app/presentation/screens/splash_screen.dart';
+import 'package:events_app/bloc/auth_bloc/auth_bloc.dart';
+
+import 'bloc/booking_bloc/book_bloc.dart';
+import 'bloc/event_bloc/event_bloc.dart';
+import 'bloc/user_bloc/user_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +16,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthBloc()),
+        BlocProvider(create: (_) => EventBloc()),
+        BlocProvider(create: (_) => BookingBloc()),
+        BlocProvider(create: (_) => UserBloc()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+        ),
+        home: const SplashScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const SplashScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
